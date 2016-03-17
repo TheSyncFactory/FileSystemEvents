67a689ece60bf4cbddc61889c02a9532259c8efb
Boy van Amstel <boyvanamstel@Boys-Mac-mini.local>
2016-03-17 15:30:25 +0100

    Switch to Swift and add basic folder monitoring

--- Project/LocalAppUpdate/AppDelegate.swift
+++ Project/LocalAppUpdate/AppDelegate.swift
@@ -13,6 +13,8 @@ import EonilFileSystemEvents
 class AppDelegate: NSObject, NSApplicationDelegate {
 
   @IBOutlet weak var window: NSWindow!
+  @IBOutlet weak var sourceButton: NSButton!
+  @IBOutlet weak var destinationButton: NSButton!
 
   var	queue	=	dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0)
   let	onEvents	=	{ (events:[FileSystemEvent]) -> () in
@@ -21,21 +23,51 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
   }
   var	monitor	=	nil as FileSystemEventMonitor?
+ 
+  private var _sourceFolder:String = ""
+  var sourceFolder:String {
+    set {
+      _sourceFolder = newValue
+      if self.watchForChanges  {
+        startMonitoring()
+      }
+    }
+    get {
+      return _sourceFolder
+    }
+  }
   
+  var destinationFolder:String = ""
+ 
   var watchForChanges:Bool {
     set {
       if newValue == true {
-        let path = "/Users/boyvanamstel/Desktop/testing"
-        self.monitor = FileSystemEventMonitor(pathsToWatch: [path], latency: 0, watchRoot: false, queue: self.queue, callback:self.onEvents)
+        startMonitoring()
       } else {
-        self.monitor = nil
+        stopMonitoring()
       }
     }
     get {
       return self.monitor != nil
     }
   }
-
+  
+  func startMonitoring() -> Bool {
+    stopMonitoring()
+    
+    let path = self.sourceFolder
+    if path.characters.count > 0 {
+      self.monitor = FileSystemEventMonitor(pathsToWatch: [path], latency: 0, watchRoot: false, queue: self.queue, callback:self.onEvents)
+      if self.monitor != nil {
+        return true
+      }
+    }
+    return false
+  }
+  func stopMonitoring() {
+    self.monitor = nil
+  }
+  
   func applicationDidFinishLaunching(aNotification: NSNotification) {
     // Insert code here to initialize your application
     
@@ -44,6 +76,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
   func applicationWillTerminate(aNotification: NSNotification) {
     // Insert code here to tear down your application
+  }
+  
+  @IBAction func switchMonitoring(sender: NSButton) {
+    
+    if self.watchForChanges {
+      self.stopMonitoring()
+      sender.state = 0
+    } else {
+      if self.startMonitoring() {
+        sender.state = 1
+      } else {
+        sender.state = 0
+      }
+    }
   }
 
 }
